fd8000aa37865a2858115c07bde3f23d0e4bd342
ia15076 <ia15076@s.inf.shizuoka.ac.jp>
2017-01-13 11:39:14 +0900

    index.phpのおすすめ書籍コーナーを小修整

--- index.php
+++ index.php
@@ -40,10 +40,10 @@
 				$stmt = $pdo->query("SELECT book.JANCode,GoogleID,ProductName,SUM(Num) AS NumSum FROM book,product,stock WHERE book.JANCode = product.JANCode AND book.JANCode = stock.JANCode AND GoogleID IS NOT NULL GROUP BY book.JANCode ORDER BY NumSum DESC LIMIT 40");
 
 				foreach ($stmt as $row) {
-					echo "<div class='book'>";
-					echo "<a href='book.php?id=$row[GoogleID]'><img alt='$row[ProductName]' src='http://books.google.com/books/content?id=$row[GoogleID]&printsec=frontcover&img=1&zoom=5&source=gbs_api'></a>";
-					echo "<a href='book.php?id=$row[GoogleID]'>$row[ProductName]</a>";
-					echo "</div>";
+					echo "<a class='book'>";
+					echo "<img alt='$row[ProductName]' src='http://books.google.com/books/content?id=$row[GoogleID]&printsec=frontcover&img=1&zoom=5&source=gbs_api'>";
+					echo "<p>$row[ProductName]</p>";
+					echo "</a>";
 				}
 			?>
 		</section>
