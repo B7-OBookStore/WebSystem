661d94387279168bdb855ea16d49dc103df723f5
Rinsz <rindoussn@outlook.jp>
2017-01-22 10:59:27 +0900

    ファイルパス変更 + registration_check.phpの表示部を実装
    (ファイルパスについてはAjaxの応答用やclassファイルのphpを/php/に移動。

---
+++ php/cls_Login.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ログイン関係の管理用クラス。
+ */
+class cls_Login
+{
+	// IDが存在するかチェックする。
+	// 存在していればtrue, 存在していなければfalseを返す。
+	function IDChecker($userid){
+
+	}
+}
--- test.php
+++ test.php
@@ -12,35 +12,43 @@ echo "this is test.<br>";
 
 <?php
 // cls_DuplicateCheck.php テスト用
-
-require 'cls_DuplicateCheck.php';
-$hoge = new cls_DuplicateCheck();
-$hoge->existPhone('sana5672@sdnpnbnh.ivx');
+//
+//
+//require 'cls_DuplicateCheck.php';
+//$hoge = new cls_DuplicateCheck();
+//$hoge->existPhone('sana5672@sdnpnbnh.ivx');
 ?>
 
 
-// 重複チェックAjax　テスト用
-<script src="js/jquery-3.1.1.min.js"></script>
-<form method="POST" action="hogehoge.php">
-	<input id="formID" type=text name="id">
-	<input id="submit" value="送信">
-	<small id="checkID">hogehoge</small>
-	<script>
-		$(function () {
-			// Ajaxによる重複チェック
+<!--// 重複チェックAjax　テスト用-->
+<!--<script src="js/jquery-3.1.1.min.js"></script>-->
+<!--<form method="POST" action="hogehoge.php">-->
+<!--	<input id="formID" type=text name="id">-->
+<!--	<input id="submit" value="送信">-->
+<!--	<small id="checkID">hogehoge</small>-->
+<!--	<script>-->
+<!--		$(function () {-->
+<!--			// Ajaxによる重複チェック-->
+<!---->
+<!--			// ID重複チェック-->
+<!--			$('input#formID').change(function () {-->
+<!--				$.get('res/res_checkID.php', {-->
+<!--					UserID: $('#formID').val()-->
+<!--				}, function (data) {-->
+<!--					$('#checkID').html(data);-->
+<!--				});-->
+<!--			});-->
+<!--		});-->
+<!--	</script>-->
+<!--</form>-->
 
-			// ID重複チェック
-			$('input#formID').change(function () {
-				$.get('res/res_checkID.php', {
-					UserID: $('#formID').val()
-				}, function (data) {
-					$('#checkID').html(data);
-				});
-			});
-		});
-	</script>
 
-</form>
+<?php
+// registration.php -> registration_check.phpの送信チェック
+echo $_POST['UserID'].'<br>';
+echo $_POST['Password'].'<br>';
+echo $_POST['Gender'].'<br>';
+?>
 
 
 
