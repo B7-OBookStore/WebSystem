49c6f68b0a702faefbd2cb57f51bff06ccd5e801
ia15076 <ia15076@s.inf.shizuoka.ac.jp>
2017-01-23 20:27:10 +0900

    数々の苦難の末ようやく注文できるように

--- ordered.php
+++ ordered.php
@@ -2,11 +2,6 @@
 	require 'php/db_connect.php';
 
 	$storeNum = $_POST['storeNum'];
-
-	$stmt = $pdo->query("SELECT StoreName FROM Store WHERE StoreNum=$storeNum");
-	if ($result = $stmt->fetch()) {
-		$storeName = $result[StoreName];
-	}
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +10,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<link rel="stylesheet" href="css/order_check.css">
+		<link rel="stylesheet" href="css/ordered.css">
 		<link rel="icon" href="img/favicon.ico">
 		<title>O書店</title>
 	</head>
@@ -34,56 +29,44 @@
 
 		<div id="main">
 			<section>
-				<h2>注文確認</h2>
-				<div id="list">
-					<h3>注文内容</h3>
-					<table>
-						<tr>
-							<th>タイトル</th>
-							<th>価格</th>
-						</tr>
-						<?php
-							$userID = $_SESSION['UserID'];
-							$stmt = $pdo->query("SELECT Book.JANCode,Price,BookTitle,Writer,GoogleID FROM Cart INNER JOIN Item ON Cart.JANCode = Item.JANCode INNER JOIN Book ON Cart.JANCode = Book.JANCode INNER JOIN User ON Cart.UserNum = User.UserNum WHERE UserID = '$userID'");
+				<div>
+				<?php
+					$userID = $_SESSION['UserID'];
+					
+					$stmt = $pdo->query("SELECT Count(*) FROM Request");
+					$requestNum = $stmt->fetchColumn();
+					
+					$sql = 'INSERT INTO Request SELECT :RequestNum,:StoreNum,NULL,FALSE,UserNum,CONCAT(LastName,FirstName),Phone,ZipCode,Pref,City,Address,Apartment FROM User WHERE UserID=:UserID';
+					$stmt = $pdo->prepare($sql);
+					$stmt->bindParam(':RequestNum',$requestNum);
+					$stmt->bindParam(':StoreNum',$storeNum);
+					$stmt->bindParam(':UserID',$userID);
+					$result = $stmt->execute();
+					
+					if ($result) {
+						$items = $pdo->query("SELECT JANCode FROM Cart INNER JOIN User ON Cart.UserNum=User.UserNum WHERE UserID='$userID'");
+						$sql = 'INSERT INTO RequestDetail VALUES(:RequestNum,:RequestDetNum,:JANCode,0)';
+						$stmt = $pdo->prepare($sql);
 
-							foreach ($stmt as $row) {
-						?>
-						<tr>
-							<td><?php echo $row[BookTitle] ?></td>
-							<td>
-							<?php
-								if ($row[Price] == NULL){
-									echo "注文確定後にお知らせ";
-								} else {
-									echo "￥ ".$row[Price];
-								}
-							?>
-							</td>
-						</tr>
-						<?php
-							}
-						?>
-					</table>
+						foreach ($items as $i => $item) {
+							$stmt->bindParam(':RequestNum',$requestNum);
+							$stmt->bindParam(':RequestDetNum',$i);
+							$stmt->bindParam(':JANCode',$item[JANCode]);
+							$stmt->execute();
+						}
 
-					<h3>受取書店</h3>
-					<p><?php echo $storeName ?></p>
+						echo '<h2>注文完了</h2>';
+						echo '<p>ご注文ありがとうございます。今後ともO書店をよろしくお願いします。</p>';
+					} else {
+						echo '<h2>注文に失敗しました</h2>';
+						echo '<p>注文処理に失敗しました。申し訳ありませんが、再度注文をよろしくお願いいたします。</p>';
+					}
+				?>
 
-					<form action="ordered.php" method="post">
-						<input type="hidden" name="storeNum" value="<?php echo $storeNum ?>">
-						<input class="button" type="submit" value="注文">
-					</form>
+				<a class="button" href="">トップに戻る</a>
 				</div>
 
-				<img id="character" alt="O書店公式キャラクター ショモツムリ" src="<?php
-					$stmt = $pdo->query("SELECT count(*) FROM Cart INNER JOIN User ON Cart.UserNum=User.UserNum WHERE UserID = '$userID'");
-					$count = $stmt->fetchColumn();
-
-					if ($count < 5) {
-						echo 'img/order_check_syomotsumuri.png';
-					} else {
-						echo 'img/order_check_syomotsumuri_heavy.png';
-					}
-				?>">
+				<img id="" alt="O書店公式キャラクター 石蕗クミコ" src="img/ordered_kumiko.png">
 			</section>
 		</div>
 
