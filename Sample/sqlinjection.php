b4787fdc2dccfd0e0acb8f6da0e499c3d55f202a
Rinsz <rindoussn@outlook.jp>
2017-01-30 10:02:46 +0900

    Commit be280f でうっかり編集後データを忘れていました…

--- Sample/sqlinjection.php
+++ Sample/sqlinjection.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: suzuno
- * Date: 17/01/30
- * Time: 2:18
- */
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="../css/information.css">
+	<link rel="icon" href="img/favicon.ico">
+	<title>O書店</title>
+</head>
+
+<body>
+
+<header>
+	<h1><a href="index.php">O書店</a></h1>
+</header>
+
+<div id="main">
+	<section>
+		<h2>SQLインジェクションテスト</h2>
+		<p>ユーザデータを表示します。</p>
+		<form method="post" action="sqlinjection.php">
+			<input type="text" name="us">
+			<input type="text" name="pa">
+			<input type="submit" value="送信">
+		</form>
+	</section>
+
+	<section>
+		<?php
+			if(!empty($_POST['us'])){
+				$us = $_POST['us'];
+				$pa = $_POST['pa'];
+
+				require '../php/db_connect.php';
+				$sql = "SELECT * FROM User WHERE UserID = '$us' AND Password = '$pa'";
+				$stmt = $pdo->query($sql);
+				echo $stmt->queryString;
+
+				while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+					echo '<p>';
+					echo 'UserID : '.$row['UserID'];
+					echo '    Password : '.$row['Password'];
+					echo '</p>';
+				}
+			}
+		?>
+	</section>
+
+
+
+	<section>
+		<h2>ヤバいコードをパスワードの部分に入力すると…</h2>
+		<p>SQLはこうなってる気がする ... SELECT * FROM User WHERE UserID = '$user' AND Password = '$pass'</p>
+		<p>例1 .. <?php echo htmlspecialchars("aaa' OR 'HOGE' = 'HOGE", ENT_QUOTES, 'UTF-8'); ?></p>
+	</section>
+
+</div>
+
+<footer>
+	<small>Copyright &copy;2016 O書店 All Rights Reserved.</small>
+</footer>
+</body>
+
+</html>
--- Sample/xss.php
+++ Sample/xss.php
@@ -1,7 +1,46 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: suzuno
- * Date: 17/01/30
- * Time: 1:40
- */
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<link rel="stylesheet" href="../css/information.css">
+		<link rel="icon" href="img/favicon.ico">
+		<title>O書店</title>
+	</head>
+
+	<body>
+
+	<header>
+		<h1><a href="index.php">O書店</a></h1>
+	</header>
+
+		<div id="main">
+			<section>
+				<h2>XSSテスト</h2>
+				<p>君の名は？</p>
+				<form method="post" action="xss.php">
+					<input type="text" name="yourname">
+					<input type="submit" value="送信">
+				</form>
+				<p>あなたの名前は <?php echo $_POST['yourname']; ?> です。</p>
+			</section>
+
+			<section>
+				<?php
+					// XSS例表示用に
+					$exp1 = htmlspecialchars('<script>alert("XSS Detected!");</script>', ENT_QUOTES, 'UTF-8');
+					$exp2 = htmlspecialchars('<script>window.location.href = "https://www.google.co.jp";</script>', ENT_QUOTES, 'UTF-8');
+				?>
+				<p>例1 ... <?php echo $exp1;?></p>
+				<p>例2 ... <?php echo $exp2;?></p>
+			</section>
+
+		</div>
+
+		<footer>
+			<small>Copyright &copy;2016 O書店 All Rights Reserved.</small>
+		</footer>
+	</body>
+
+</html>
